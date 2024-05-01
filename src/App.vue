8997b8b13b4552fa56cb0e57899c57b53cf446ff
palmuccivalerio <palmuccivalerio@gmail.com>
2024-05-01 17:39:33 +0200

    add:title

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+
+
+export default {
+  components: {
+    AppTitle,
+    
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  
+    <AppTitle />
+  
+</template>
+
+<style></style>  
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "IL MIO PRIMO TITOLO VUE "
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+
+</template>
+
+<style></style>
